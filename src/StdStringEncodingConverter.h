e36a84c9c98779e0f3df469c4c8802d0c62b6fa3
Jan Heberer <jangc@t-online.de>
2017-05-16 18:25:44 +0200

    Add class for conversion between string encodings

---
+++ src/StdStringEncodingConverter.h
@@ -0,0 +1,31 @@
+/*
+ * LegacyClonk
+ *
+ * Copyright (c) 2017, The LegacyClonk Team and contributors
+ *
+ * Distributed under the terms of the ISC license; see accompanying file
+ * "COPYING" for details.
+ *
+ * "Clonk" is a registered trademark of Matthes Bender, used with permission.
+ * See accompanying file "TRADEMARK" for details.
+ *
+ * To redistribute this file separately, substitute the full license texts
+ * for the above references.
+ */
+
+#pragma once
+
+#ifdef WIN32
+
+#include <Windows.h>
+#include <string>
+
+class StdStringEncodingConverter
+{
+public:
+	/* Converts a string encoded in the system default ACP to UTF-16.
+	   If "last" is nullptr, the string specified by "first" must be null-terminated. */
+	std::wstring WinAcpToUtf16(LPCCH first, LPCCH last = nullptr) const;
+};
+
+#endif
