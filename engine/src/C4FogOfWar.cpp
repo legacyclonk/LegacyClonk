67a4d0f82a46b4b29975bfaa0e65b9657ff973b4
Jan Heberer <jangc@t-online.de>
2017-05-16 18:23:54 +0200

    Remove obsolete files
    - project files
    - build instructions
    - version files: build_tag, version
    - anigrab
    - editor
    - netpuncher
    - source code of: FreeType, libjpeg, libpng, OpenSSL
    - fmod files
    - glew files
    - Unused source files: d3dutil.cpp, d3dutil.h, dxutil.cpp, dxutil.h, DInputX.cpp, DInputX.h, DSoundX.cpp, DSoundX.h, PathFinder.cpp, PathFinder.h, StdBase64.cpp, StdBase64.h, StdBitmap.cpp, StdBitmap.h
    - Virtually empty file C4FogOfWar.cpp
    - Unused resource files: Game.pal and Grab.bmp

--- engine/src/C4FogOfWar.cpp
+++
@@ -1,7 +0,0 @@
-/* Copyright (C) 1998-2000  Matthes Bender  RedWolf Design */
-
-/* A visibility map for each player */
-
-#include <C4Include.h>
-#include <C4FogOfWar.h>
-
--- standard/inc/StdBase64.h
+++
@@ -1,6 +0,0 @@
-
-/* Base64 encoding / decoding */
-
-
-char *B64Encode(const void *pData, int iSize);
-char *B64Decode(const char *pData, int &ioSize);
