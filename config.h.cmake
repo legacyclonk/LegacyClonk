e91aa84e1507abd64c3f3f23088a60880ff751b6
Jan Heberer <jangc@t-online.de>
2017-05-16 18:24:11 +0200

    Create CMake files

---
+++ config.h.cmake
@@ -0,0 +1,33 @@
+#cmakedefine DEBUGREC 1
+#cmakedefine USE_CONSOLE 1
+#cmakedefine USE_DIRECTX 1
+#cmakedefine USE_FMOD 1
+#cmakedefine USE_GL 1
+#cmakedefine USE_MP3 1
+#cmakedefine USE_SDL_MAINLOOP 1
+#cmakedefine USE_X11 1
+#cmakedefine WITH_DEVELOPER_MODE 1
+#cmakedefine WITH_GLIB 1
+
+#cmakedefine HAVE_DIRECT_H 1
+#cmakedefine HAVE_EXECINFO_H 1
+#cmakedefine HAVE_FREETYPE 1
+#cmakedefine HAVE_ICONV 1
+#cmakedefine HAVE_IO_H 1
+#cmakedefine HAVE_LANGINFO_H 1
+#cmakedefine HAVE_LIBSDL_MIXER 1
+#cmakedefine HAVE_LOCALE_H 1
+#cmakedefine HAVE_PTHREAD 1
+#cmakedefine HAVE_SDL 1
+#cmakedefine HAVE_SHARE_H 1
+#cmakedefine HAVE_SIGNAL_H 1
+#cmakedefine HAVE_STDINT_H 1
+#cmakedefine HAVE_SYS_INOTIFY_H 1
+#cmakedefine HAVE_SYS_SOCKET_H 1
+#cmakedefine HAVE_SYS_STAT_H 1
+#cmakedefine HAVE_SYS_SYSCALL_H 1
+#cmakedefine HAVE_SYS_TYPES_H 1
+#cmakedefine HAVE_UNISTD_H 1
+#cmakedefine HAVE_VASPRINTF 1
+
+#define ICONV_CONST
