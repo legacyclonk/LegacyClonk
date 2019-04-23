54da6e55e70b19d699aa279553263876bec888a5
Markus Mittendrein <git@maxmitti.tk>
2019-04-23 19:28:22 +0200

    Add "LegacyClonk" into C4VERSIONEXTRA

--- src/C4Version.h
+++ src/C4Version.h
@@ -27,7 +27,7 @@
 #define C4XVER3 10
 #define C4XVER4 7
 #define C4XVERBUILD 330
-#define C4VERSIONEXTRA ""
+#define C4VERSIONEXTRA " LegacyClonk"
 /* These values are now controlled by the file source/version - DO NOT MODIFY DIRECTLY */
 
 // Build Options
