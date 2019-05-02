92cad9b53ffd89aeff13b5c82b975ce1e2e9b31e
Markus Mittendrein <git@maxmitti.tk>
2019-05-03 01:06:04 +0200

    Fix invalid reads in C4KeyboardInput::LoadCustomConfig (and simplify code)

--- src/C4KeyboardInput.cpp
+++ src/C4KeyboardInput.cpp
@@ -819,9 +819,8 @@ bool C4KeyboardInput::LoadCustomConfig()
 	// load from INI file (2do: load from registry)
 	C4Group GrpExtra;
 	if (!GrpExtra.Open(C4CFN_Extra)) return false;
-	StdBuf sFileContents;
-	if (!GrpExtra.LoadEntry(C4CFN_KeyConfig, sFileContents)) return false;
-	StdStrBuf sFileContentsString((const char *)sFileContents.getData());
+	StdStrBuf sFileContentsString;
+	if (!GrpExtra.LoadEntryString(C4CFN_KeyConfig, sFileContentsString)) return false;
 	if (!CompileFromBuf_LogWarn<StdCompilerINIRead>(*this, sFileContentsString, "Custom keys from" C4CFN_Extra DirSep C4CFN_KeyConfig))
 		return false;
 	LogF(LoadResStr("IDS_PRC_LOADEDKEYCONF"), C4CFN_Extra DirSep C4CFN_KeyConfig);
