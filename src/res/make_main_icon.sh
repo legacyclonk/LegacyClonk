854705b6d5026407a900ea7bdb1f2465a2029b4b
Markus Mittendrein <git@maxmitti.tk>
2019-05-03 01:46:22 +0200

    Add .icns variant of the new icon (also in make_main_icon.sh)

--- src/res/make_main_icon.sh
+++ src/res/make_main_icon.sh
@@ -2,3 +2,12 @@
 convert +antialias -background none -density 384 lc.svg -define icon:auto-resize lc.ico
 convert +antialias -background none -density 384 lc.svg lc.xpm
 sed -i -E 's/^static char \*lc\[\] = \{$/static const char *c4x_xpm[] = {/' lc.xpm
+
+mkdir -p icns_src
+pushd icns_src
+for density in 24 48 192 384 767 1536; do
+	convert +antialias -background none -density $density ../lc.svg lc_$density.png
+done
+png2icns ../lc.icns *.png
+popd
+rm -r icns_src
