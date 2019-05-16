21eddbfd9aba4f527485d14bdee4026f60389991
Markus Mittendrein <git@maxmitti.tk>
2019-05-16 22:59:06 +0200

    Add make_Graphics.c4g.sh

---
+++ tools/make_Graphics.c4g.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+set -e
+
+if [ -z "$C4GROUP" ]; then
+	echo "Please define the environment variable C4GROUP to point to your c4group(64) binary" >&2
+	exit 1
+fi
+
+SCRIPT_DIR="$(realpath $(dirname $0))"
+
+if [ -z "$SRCDIR" ]; then
+	SRCDIR="$SCRIPT_DIR/../planet/Graphics.c4g"
+fi
+
+if [ -f Graphics.c4g ]; then
+	rm Graphics.c4g
+fi
+
+cp -r "$SRCDIR" .
+
+"$C4GROUP" Graphics.c4g -p
