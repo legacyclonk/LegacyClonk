eeecf6ee76dcbd85bd28c31df6d1738609684fb7
Fulgen301 <tokmajigeorge@gmail.com>
2019-06-16 20:45:07 +0200

    Bump to 332

--- src/C4Version.h
+++ src/C4Version.h
@@ -26,7 +26,7 @@
 #define C4XVER2 9
 #define C4XVER3 10
 #define C4XVER4 7
-#define C4XVERBUILD 331
+#define C4XVERBUILD 332
 #define C4VERSIONEXTRA " LegacyClonk"
 /* These values are now controlled by the file source/version - DO NOT MODIFY DIRECTLY */
 
