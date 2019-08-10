1b2421e9223477f53c50608c8cbb18368852df6b
Fulgen301 <tokmajigeorge@gmail.com>
2019-08-10 14:42:49 +0200

    Update version to 334

--- src/C4Version.h
+++ src/C4Version.h
@@ -29,7 +29,7 @@
 #define C4XVER2 9
 #define C4XVER3 10
 #define C4XVER4 7
-#define C4XVERBUILD 333
+#define C4XVERBUILD 334
 #define C4VERSIONEXTRA " LegacyClonk"
 /* These values are now controlled by the file source/version - DO NOT MODIFY DIRECTLY */
 
