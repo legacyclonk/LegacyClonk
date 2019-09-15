43ae12966a8b3e0ca1fc8a5a8c0c8119643489d3
Markus Mittendrein <git@maxmitti.tk>
2019-09-15 14:36:34 +0200

    planet/System.c4g/C4.c: Use the ?. operator to further simplify the GetPathLength() implementation

--- planet/System.c4g/C4.c
+++ planet/System.c4g/C4.c
@@ -97,6 +97,5 @@ global func BarrelConfiguration() { return 5; }
 
 global func GetPathLength(int fromX, int fromY, int toX, int toY)
 {
-  var path = GetPath(fromX, fromY, toX, toY);
-  return path && path.Length;
+  return GetPath(fromX, fromY, toX, toY)?.Length;
 }
