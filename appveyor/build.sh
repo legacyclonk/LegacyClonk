2d105e68961c2253e7ae76a26b0c4102a40e95e0
Markus Mittendrein <git@maxmitti.tk>
2019-09-17 21:23:22 +0200

    Simplify and optimize the linux parts of appveyor.yml
    - Replace many adjacent test "$PLATFORM" by one if [] [else] fi block
    - Change the x86 chroot archive compression to xz
    - Use a ramdisk for the x86 chroot and build
    - Use a ramdisk for the x64 build

--- appveyor/build.sh
+++ appveyor/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 sudo DEBIAN_FRONTEND=noninteractive apt-get install zlib1g-dev build-essential libgl1-mesa-dev libjpeg-dev libpng-dev libssl-dev libglu1-mesa-dev libglew-dev libsdl1.2-dev libsdl-mixer1.2-dev libfreetype6-dev freeglut3-dev libxpm-dev cmake make git -y
 sudo update-alternatives --set gcc /usr/bin/gcc-9
-git status || cd /legacyclonk
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX="/usr" -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
 make -j$(nproc)
-test -e clonk && tar -cvzf LegacyClonk-${PLATFORM}.tar.gz clonk c4group
+test -e clonk && tar -cvzf LegacyClonk.tar.gz clonk c4group
