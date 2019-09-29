01369f4afa294888526b8cd833336fdb87d4281e
Markus Mittendrein <git@maxmitti.tk>
2019-09-29 21:42:37 +0200

    Linux autobuilds: Build with WITH_DEVELOPER_MODE to actually get a working /console

--- appveyor/build.sh
+++ appveyor/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-sudo DEBIAN_FRONTEND=noninteractive apt-get install zlib1g-dev build-essential libgl1-mesa-dev libjpeg-dev libpng-dev libssl-dev libglu1-mesa-dev libglew-dev libsdl1.2-dev libsdl-mixer1.2-dev libfreetype6-dev freeglut3-dev libxpm-dev cmake make git -y
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends zlib1g-dev libgl1-mesa-dev libjpeg-dev libpng-dev libssl-dev libglu1-mesa-dev libglew-dev libsdl1.2-dev libsdl-mixer1.2-dev libfreetype6-dev freeglut3-dev libxpm-dev libgtk2.0-dev cmake make git -y
 sudo update-alternatives --set gcc /usr/bin/gcc-9
+sudo update-alternatives --set g++ /usr/bin/g++-9
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX="/usr" -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+cmake -DCMAKE_INSTALL_PREFIX="/usr" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_DEVELOPER_MODE=On ..
 make -j$(nproc)
 test -e clonk && tar -cvzf LegacyClonk.tar.gz clonk c4group
