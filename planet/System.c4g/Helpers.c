2cdfb145a902469335575cc4ffc849a10f184850
Markus Mittendrein <git@maxmitti.tk>
2019-10-10 22:50:10 +0200

    Fix planet/System.c4g/Helpers.c

--- planet/System.c4g/Helpers.c
+++ planet/System.c4g/Helpers.c
@@ -10,18 +10,18 @@ global func MessageWindow(string pMsg, int iForPlr, id idIcon, string pCaption)
 	if (!pCaption) pCaption=GetName();
 	// create msg window (menu)
 	var pCursor = GetCursor(iForPlr);
-	if (!CreateMenu(idIcon, pCursor, pCursor, 0, pCaption, 0, 2)) return;
-	AddMenuItem(pCaption, "", TIM1, pCursor,0,0,pMsg);
+	if (!CreateMenu(idIcon, pCursor, pCursor, nil, pCaption, nil, C4MN_Style_Info)) return;
+	AddMenuItem(pCaption, "", TIM1, pCursor, nil, nil,pMsg);
 	return true;
 }
 
 global func RemoveAll(id idDef, int dwOCF)
 {
-	var Cnt, obj = FindObject(idDef, 0,0,0,0, dwOCF), next;
+	var Cnt, obj = FindObject(idDef, 0,0,nil,nil, dwOCF), next;
 	while (obj)
 	{
 		// Get the next object in case obj->Destruction does funny things
-		next = FindObject(idDef, 0,0,0,0, dwOCF, 0,0,0, obj);
+		next = FindObject(idDef, 0,0,nil,nil, dwOCF, nil,nil,nil, obj);
 		if (RemoveObject(obj))
 			++Cnt;
 		obj = next;
@@ -59,7 +59,7 @@ global func ToggleBit(int iOldVal, int iBitNr)
 global func RGB(int r, int g, int b) { return (r & 255) << 16 | (g & 255) << 8 | (b & 255); }
 global func RGBa(int r, int g, int b, int a) { return (a & 255) << 24 | (r & 255) << 16 | (g & 255) << 8 | (b & 255); }
 
-global func DrawParticleLine(szKind, x0, y0, x1, y1, prtdist, a, b0, b1, ydir)
+global func DrawParticleLine(string szKind, int x0, int y0, int x1, int y1, int prtdist, int a, int b0, int b1, int ydir)
 {
 	// Parameter gültig?
 	if (!prtdist) return nil;
@@ -76,26 +76,26 @@ global func DrawParticleLine(szKind, x0, y0, x1, y1, prtdist, a, b0, b1, ydir)
 		b = ((b0 & 16711935) * i1 + (b1 & 16711935) * i2) >> 8 & 16711935
 		| ((b0 >> 8 & 16711935) * i1 + (b1 >> 8 & 16711935) * i2) & -16711936;
 		if (!b && (b0 | b1)) ++b;
-		CreateParticle(szKind, x0 + (x1 - x0) *i / prtnum, y0 + (y1 - y0) * i-- / prtnum, 0, ydir, a, b);
+		CreateParticle(szKind, x0 + (x1 - x0) *i / prtnum, y0 + (y1 - y0) * i-- / prtnum, nil, ydir, a, b);
 	}
 	// Erfolg; Anzahl erzeugter Partikel zurückgeben
 	return prtnum;
 }
 
 //Such nach einem Objekt, das ein Acquire-Command holen könnte
-global func GetAvailableObject(def, xobj)
+global func GetAvailableObject(id def, object xobj)
 {
 	var obj;
 	// Next closest
-	while (obj = FindObject(def, 0, 0, -1, -1, OCF_Available, 0, 0, 0, obj))
+	while (obj = FindObject(def, 0, 0, -1, -1, OCF_Available, nil, nil, nil, obj))
 	{
 		if (ObjectDistance(obj) > 559) return;
 		// Object is near enough
 		if (!Inside (GetX() - GetX(obj), -500, +500)) continue;
 		if (!Inside (GetY() - GetY(obj), -250, +250)) continue;
 		// Object is not connected to a pipe (for line construction kits)
-		if (FindObject(SPIP, 0, 0, 0, 0, 0, "Connect", obj)) continue;
-		if (FindObject(DPIP, 0, 0, 0, 0, 0, "Connect", obj)) continue;
+		if (FindObject(SPIP, 0, 0, nil, nil, nil, "Connect", obj)) continue;
+		if (FindObject(DPIP, 0, 0, nil, nil, nil, "Connect", obj)) continue;
 		// Must be complete
 		if (~GetOCF(obj) & OCF_Fullcon) continue;
 		// Doesn't burn
