cfdb53e550619e4da2d695bf78075cf949c77917
Markus Mittendrein <git@maxmitti.tk>
2020-03-05 21:11:29 +0100

    Bump Objects version to 4.9.10.9

--- src/C4Version.h
+++ src/C4Version.h
@@ -28,7 +28,7 @@
 #define C4XVER1 4
 #define C4XVER2 9
 #define C4XVER3 10
-#define C4XVER4 8
+#define C4XVER4 9
 #define C4XVERBUILD 339
 #define C4VERSIONEXTRA ""
 /* These values are now controlled by the file source/version - DO NOT MODIFY DIRECTLY */
