6679aa66f037e946d8f7815783d3612adbcbc393
George Tokmaji <tokmajigeorge@gmail.com>
2020-05-27 14:58:00 +0200

    CStdDDraw: Remove unused memberr

--- src/StdDDraw2.h
+++ src/StdDDraw2.h
@@ -324,7 +324,7 @@ public:
 	};
 
 public:
-	CStdDDraw() : Saturation(255) { lpDDrawPal = &Pal; }
+	CStdDDraw() { lpDDrawPal = &Pal; }
 	virtual ~CStdDDraw() { lpDDraw = nullptr; }
 
 public:
@@ -347,7 +347,6 @@ protected:
 	uint32_t dwBlitMode; // extra flags for blit
 	CClrModAddMap *pClrModMap; // map to be used for global color modulation (invalid if !fUseClrModMap)
 	bool fUseClrModMap; // if set, pClrModMap will be checked for color modulations
-	unsigned char Saturation; // if < 255, an extra filter is used to reduce the saturation
 
 	std::stack<DrawMode> modes;
 	std::unordered_map<DrawMode, CStdShaderProgram *> modeShaders;
