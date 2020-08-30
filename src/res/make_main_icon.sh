ebd2b9f4af89b1c0b9d0b22fa6d39f1618ab0b39
Markus Mittendrein <git@maxmitti.tk>
2020-08-30 21:08:05 +0200

    Reenable generation of lc.icns and add it

--- src/res/make_main_icon.sh
+++ src/res/make_main_icon.sh
@@ -3,11 +3,11 @@ convert +antialias -background none -density 1024 lc.png -define icon:auto-resiz
 convert +antialias -background none -resize 256x256 lc.png lc.xpm
 sed -i -E 's/^static char \*lc\[\] = \{$/static const char *c4x_xpm[] = {/' lc.xpm
 
-# mkdir -p icns_src
-# pushd icns_src
-# for resolution in 24 48 192 384 767 1536; do
-# 	convert -resize ${resolution}x${resolution} ../lc.png lc_$resolution.png
-# done
-# png2icns ../lc.icns *.png
-# popd
-# rm -r icns_src
+mkdir -p icns_src
+pushd icns_src
+for resolution in 16 32 48 128 256 512 1024; do
+	convert -resize ${resolution}x${resolution} ../lc.png lc_$resolution.png
+done
+png2icns ../lc.icns *.png
+popd
+rm -r icns_src
