f267d03bcdbff18905fe5ae86d227a9942635c91
George Tokmaji <tokmajigeorge@gmail.com>
2020-12-30 19:05:57 +0100

    Do not brick third-party GOLD objects which don't have BaseAutoSell set

--- src/C4Def.cpp
+++ src/C4Def.cpp
@@ -467,7 +467,7 @@ void C4DefCore::CompileFunc(StdCompiler *pComp)
 	pComp->Value(mkNamingAdapt(mkBitfieldAdapt<int32_t>(HideHUDElements, HideHUDValues), "HideHUDElements", 0));
 
 	pComp->Value(mkNamingAdapt(Scale, "Scale", 100));
-	pComp->Value(mkNamingAdapt(BaseAutoSell, "BaseAutoSell", false));
+	pComp->Value(mkNamingAdapt(BaseAutoSell, "BaseAutoSell", id == C4ID_Gold)); // do not brick third-party GOLD objects which don't have that flag
 
 	pComp->FollowName("Physical");
 	pComp->Value(Physical);
