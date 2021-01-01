e5b478eb4e9539f48feb8cdfed485584ef8cf720
George Tokmaji <tokmajigeorge@gmail.com>
2021-01-01 01:42:09 +0100

    Merge StdJoystick and C4GamePad

--- src/C4GamePadCon.h
+++ src/C4GamePadCon.h
@@ -19,7 +19,11 @@
 #pragma once
 
 #ifdef _WIN32
-#include <StdJoystick.h>
+#include "C4Windows.h"
+#include <mmsystem.h>
+
+#include <array>
+#include <cinttypes>
 #endif
 
 #ifdef USE_SDL_FOR_GAMEPAD
@@ -35,19 +39,55 @@ typedef struct _SDL_Joystick SDL_Joystick;
 union SDL_Event;
 typedef union SDL_Event SDL_Event;
 
+#ifdef _WIN32
+
+class C4GamePad
+{
+public:
+	enum AxisPos { Low, Mid, High, }; // quantized axis positions
+	enum AxisPOV { X = 6, Y = 7 }; // virtual axises of the coolie hat
+
+	static constexpr int32_t MaxGamePad{15}, // maximum number of supported gamepads
+							 MaxCalAxis{6},  // maximum number of calibrated axises
+							 MaxAxis   {8};  // number of axises plus coolie hat axises
+
+public:
+	C4GamePad(int id);
+	~C4GamePad();
+
+public:
+	void SetCalibration(uint32_t *pdwAxisMin, uint32_t *pdwAxisMax, bool *pfAxisCalibrated);
+	void GetCalibration(uint32_t *pdwAxisMin, uint32_t *pdwAxisMax, bool *pfAxisCalibrated);
+
+	bool Update(); // read current gamepad data
+	uint32_t GetCurrentButtons(); // returns bitmask of pressed buttons for last retrieved info
+	AxisPos GetAxisPos(int idAxis); // return axis extension - mid for error or center position
+
+	void IncRef();
+	bool DecRef();
+	int32_t GetID() const { return id; }
+
+public:
+	std::array<uint32_t, MaxCalAxis> dwAxisMin;
+	std::array<uint32_t, MaxCalAxis> dwAxisMax; // axis ranges - auto calibrated
+	std::array<bool, MaxCalAxis> fAxisCalibrated; // set if an initial value for axis borders has been determined already
+	std::array<AxisPos, MaxAxis> AxisPosis;
+
+	int iRefCount;
+	uint32_t Buttons;
+
+private:
+	int id; // gamepad number
+	JOYINFOEX joynfo; // WIN32 gamepad info
+};
+#endif
+
 class C4GamePadControl
 {
 #ifdef _WIN32
 
 private:
-	struct Pad
-	{
-		CStdGamePad *pGamepad;
-		int iRefCount;
-		uint32_t Buttons;
-		CStdGamePad::AxisPos AxisPosis[CStdGamepad_MaxAxis];
-	};
-	Pad Gamepads[CStdGamepad_MaxGamePad];
+	std::array<C4GamePad *, C4GamePad::MaxGamePad> Gamepads{};
 	int iNumGamepads;
 
 public:
