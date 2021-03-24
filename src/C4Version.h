6155bb7a4966396589fc9c2d67cba2e01f57b5ff
Markus Mittendrein <github@maxmitti.tk>
2021-03-24 23:13:05 +0100

    Bump version to 347

--- src/C4Version.h
+++ src/C4Version.h
@@ -29,7 +29,7 @@
 #define C4XVER2 9
 #define C4XVER3 10
 #define C4XVER4 11
-#define C4XVERBUILD 346
+#define C4XVERBUILD 347
 #define C4VERSIONEXTRA ""
 /* These values are now controlled by the file source/version - DO NOT MODIFY DIRECTLY */
 
