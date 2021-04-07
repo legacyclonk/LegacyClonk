551f2c9bfa21c2f1f39e87a27933170e5d74dfff
Markus Mittendrein <github@maxmitti.tk>
2021-04-07 20:30:43 +0200

    Autobuild: Add USE_FMOD for Windows x64

--- autobuild/Windows-x86.sh
+++ autobuild/Windows-x86.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "CMAKE_CONFIGURE_ARGS=-A Win32 -C $PWD/config.txt -DUSE_FMOD=ON" >> $GITHUB_ENV
+echo "CMAKE_CONFIGURE_ARGS=$CMAKE_CONFIGURE_ARGS -A Win32 -C $PWD/config.txt" >> $GITHUB_ENV
 echo "VS_ARCH=x86" >> $GITHUB_ENV
