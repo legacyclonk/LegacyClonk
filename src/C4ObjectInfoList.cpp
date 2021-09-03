legacyclonk/LegacyClonk

commit 92a5480c308095d7bf9b74ce5993d2c81e4dcb79

Replace checks for operator new() != nullptr by unique_ptr

src/C4ObjectInfoList.cpp
@@ -55,27 +55,32 @@ void C4ObjectInfoList::Clear()
 
 int32_t C4ObjectInfoList::Load(C4Group &hGroup, bool fLoadPortraits)
 {
-	C4ObjectInfo *ninf;
+	std::unique_ptr<C4ObjectInfo> info;
 	int32_t infn = 0;
 	char entryname[256 + 1];
 
 	// Search all c4i files
 	hGroup.ResetSearch();
 	while (hGroup.FindNextEntry(C4CFN_ObjectInfoFiles, entryname))
-		if (ninf = new C4ObjectInfo)
-			if (ninf->Load(hGroup, entryname, fLoadPortraits)) { Add(ninf); infn++; }
-			else delete ninf;
+	{
+		info.reset(new C4ObjectInfo);
+		if (info->Load(hGroup, entryname, fLoadPortraits))
+		{
+			Add(info.release());
+			++infn;
+		}
 
-			// Search subfolders
-			hGroup.ResetSearch();
-			while (hGroup.FindNextEntry("*", entryname))
-			{
-				C4Group ItemGroup;
-				if (ItemGroup.OpenAsChild(&hGroup, entryname))
-					Load(ItemGroup, fLoadPortraits);
-			}
+		// Search subfolders
+		hGroup.ResetSearch();
+		while (hGroup.FindNextEntry("*", entryname))
+		{
+			C4Group ItemGroup;
+			if (ItemGroup.OpenAsChild(&hGroup, entryname))
+				Load(ItemGroup, fLoadPortraits);
+		}
+	}
 
-			return infn;
+	return infn;
 }
 
 bool C4ObjectInfoList::Add(C4ObjectInfo *pInfo)
@@ -139,33 +144,45 @@ C4ObjectInfo *C4ObjectInfoList::GetIdle(C4ID c_id, C4DefList &rDefs)
 
 C4ObjectInfo *C4ObjectInfoList::New(C4ID n_id, C4DefList *pDefs, const char *cpNames)
 {
-	C4ObjectInfo *pInfo;
 	// Create new info object
-	if (!(pInfo = new C4ObjectInfo)) return nullptr;
+	auto info = std::make_unique<C4ObjectInfo>();
+
 	// Default type clonk if none specified
 	if (n_id == C4ID_None) n_id = C4ID_Clonk;
+
 	// Check type valid and def available
-	C4Def *pDef = nullptr;
-	if (!pDefs || !(pDef = pDefs->ID2Def(n_id)))
+	C4Def *def{nullptr};
+	if (!pDefs || !(def = pDefs->ID2Def(n_id)))
 	{
-		delete pInfo; return nullptr;
+		return nullptr;
 	}
+
 	// Override name source by definition
-	if (pDef->pClonkNames)
-		cpNames = pDef->pClonkNames->GetData();
+	if (def->pClonkNames)
+	{
+		cpNames = def->pClonkNames->GetData();
+	}
+
 	// Default by type
-	static_cast<C4ObjectInfoCore *>(pInfo)->Default(n_id, pDefs, cpNames);
+	static_cast<C4ObjectInfoCore *>(info.get())->Default(n_id, pDefs, cpNames);
+
 	// Set birthday
-	pInfo->Birthday = static_cast<int32_t>(time(nullptr));
+	info->Birthday = static_cast<int32_t>(time(nullptr));
+
 	// Make valid names
-	MakeValidName(pInfo->Name);
+	MakeValidName(info->Name);
+
 	// Add new portrait (permanently w/o copying file)
 	if (Config.Graphics.AddNewCrewPortraits)
-		pInfo->SetRandomPortrait(0, true, false);
+	{
+		info->SetRandomPortrait(0, true, false);
+	}
+
 	// Add
-	Add(pInfo);
+	Add(info.get());
 	++iNumCreated;
-	return pInfo;
+
+	return info.release();
 }
 
 void C4ObjectInfoList::Evaluate()

src/C4Def.cpp
@@ -949,7 +949,6 @@ int32_t C4DefList::Load(C4Group &hGroup, uint32_t dwLoadWhat,
 	bool fSearchMessage, int32_t iMinProgress, int32_t iMaxProgress, bool fLoadSysGroups)
 {
 	int32_t iResult = 0;
-	C4Def *nDef;
 	char szEntryname[_MAX_FNAME + 1];
 	C4Group hChild;
 	bool fPrimaryDef = false;
@@ -969,16 +968,17 @@ int32_t C4DefList::Load(C4Group &hGroup, uint32_t dwLoadWhat,
 	if (fThisSearchMessage) { LogF("%s...", GetFilename(hGroup.GetName())); }
 #endif
 
+	auto def = std::make_unique<C4Def>();
 	// Load primary definition
-	if (nDef = new C4Def)
-		if (nDef->Load(hGroup, dwLoadWhat, szLanguage, pSoundSystem) && Add(nDef, fOverload))
-		{
-			iResult++; fPrimaryDef = true;
-		}
-		else
-		{
-			delete nDef;
-		}
+	if (def->Load(hGroup, dwLoadWhat, szLanguage, pSoundSystem) && Add(def.get(), fOverload))
+	{
+		iResult++; fPrimaryDef = true;
+		def.release();
+	}
+	else
+	{
+		def.reset();
+	}
 
 	// Load sub definitions
 	int i = 0;

src/C4Game.cpp
@@ -1076,26 +1076,35 @@ C4Object *C4Game::NewObject(C4Def *pDef, C4Object *pCreator,
 	AddDbgRec(RCT_CrObj, &rc, sizeof(rc));
 #endif
 	// Create object
-	C4Object *pObj;
-	if (!(pObj = new C4Object)) return nullptr;
+	auto obj = std::make_unique<C4Object>();
+	C4Object *const objPtr{obj.get()};
+
 	// Initialize object
-	pObj->Init(pDef, pCreator, iOwner, pInfo, iX, iY, iR, xdir, ydir, rdir, iController);
+	obj->Init(pDef, pCreator, iOwner, pInfo, iX, iY, iR, xdir, ydir, rdir, iController);
 	// Enumerate object
-	pObj->Number = ++ObjectEnumerationIndex;
+	obj->Number = ++ObjectEnumerationIndex;
 	// Add to object list
-	if (!Objects.Add(pObj)) { delete pObj; return nullptr; }
+	if (Objects.Add(obj.get()))
+	{
+		obj.release();
+	}
+	else
+	{
+		return nullptr;
+	}
+
 	// ---- From now on, object is ready to be used in scripts!
 	// Construction callback
 	C4AulParSet pars(C4VObj(pCreator));
-	pObj->Call(PSF_Construction, pars);
+	objPtr->Call(PSF_Construction, pars);
 	// AssignRemoval called? (Con 0)
-	if (!pObj->Status) { return nullptr; }
+	if (!objPtr->Status) { return nullptr; }
 	// Do initial con
-	pObj->DoCon(iCon, true);
+	objPtr->DoCon(iCon, true);
 	// AssignRemoval called? (Con 0)
-	if (!pObj->Status) { return nullptr; }
+	if (!objPtr->Status) { return nullptr; }
 	// Success
-	return pObj;
+	return objPtr;
 }
 
 void C4Game::DeleteObjects(bool fDeleteInactive)
@@ -2034,20 +2043,25 @@ bool C4Game::SaveGameTitle(C4Group &hGroup)
 	// Fullscreen screenshot
 	else if (Application.isFullScreen && Application.Active)
 	{
-		C4Surface *sfcPic; int32_t iSfcWdt = 200, iSfcHgt = 150;
-		if (!(sfcPic = new C4Surface(iSfcWdt, iSfcHgt))) return false;
+		constexpr int32_t surfaceWidth{200};
+		constexpr int32_t surfaceHeight{150};
+
+		const auto surface = std::make_unique<C4Surface>(surfaceWidth, surfaceHeight);
 
 		// Fullscreen
 		Application.DDraw->Blit(Application.DDraw->lpBack,
 			0.0f, 0.0f, float(Application.DDraw->lpBack->Wdt), float(Application.DDraw->lpBack->Hgt),
-			sfcPic, 0, 0, iSfcWdt, iSfcHgt);
+			surface.get(), 0, 0, surfaceWidth, surfaceHeight);
 
-		bool fOkay = true;
-		const char *szDestFn;
-		fOkay = sfcPic->SavePNG(Config.AtTempPath(C4CFN_TempTitle), false, true, false);
-		szDestFn = C4CFN_ScenarioTitlePNG;
-		delete sfcPic; if (!fOkay) return false;
-		if (!hGroup.Move(Config.AtTempPath(C4CFN_TempTitle), szDestFn)) return false;
+		if (!surface->SavePNG(Config.AtTempPath(C4CFN_TempTitle), false, true, false))
+		{
+			return false;
+		}
+
+		if (!hGroup.Move(Config.AtTempPath(C4CFN_TempTitle), C4CFN_ScenarioTitlePNG))
+		{
+			return false;
+		}
 	}
 
 	return true;

src/C4Group.cpp
@@ -684,15 +684,15 @@ bool C4Group::Open(const char *szGroupName, bool fCreate)
 	} while (!FileExists(szRealGroup));
 
 	// Open mother and child in exclusive mode
-	C4Group *pMother;
-	if (!(pMother = new C4Group))
-		return Error("Open: mem");
-	pMother->SetStdOutput(StdOutput);
-	if (!pMother->Open(szRealGroup))
+	auto *const mother = new C4Group;
+	mother->SetStdOutput(StdOutput);
+
+	if (!mother->Open(szRealGroup))
 	{
 		Clear(); return Error("Open: Cannot open mother");
 	}
-	if (!OpenAsChild(pMother, szGroupNameN + SLen(szRealGroup) + 1, true))
+
+	if (!OpenAsChild(mother, szGroupNameN + SLen(szRealGroup) + 1, true))
 	{
 		Clear(); return Error("Open:: Cannot open as child");
 	}
@@ -831,7 +831,8 @@ bool C4Group::AddEntry(int status,
 	if (centry) { centry->Status = C4GRES_Deleted; Head.Entries--; }
 
 	// Allocate memory for new entry
-	if (!(nentry = new C4GroupEntry)) return false; // ...theoretically, delete Hold buffer here
+	nentry = new C4GroupEntry;
+	// if (!(nentry = new C4GroupEntry)) return false; ...theoretically, delete Hold buffer here - why?
 
 	// Find end of list
 	for (lentry = FirstEntry; lentry && lentry->Next; lentry = lentry->Next);
@@ -2194,7 +2195,8 @@ bool C4Group::LoadEntry(const char *szEntryName, char **lpbpBuf, size_t *ipSize,
 	// Access entry, allocate buffer, read data
 	(*lpbpBuf) = nullptr; if (ipSize) *ipSize = 0;
 	if (!AccessEntry(szEntryName, &size)) return Error("LoadEntry: Not found");
-	if (!((*lpbpBuf) = new char[size + iAppendZeros])) return Error("LoadEntry: Insufficient memory");
+
+	*lpbpBuf = new char[size + iAppendZeros];
 	if (!Read(*lpbpBuf, size))
 	{
 		delete[] (*lpbpBuf); *lpbpBuf = nullptr;

src/C4Landscape.cpp
@@ -509,21 +509,20 @@ bool C4Landscape::MapToLandscape(CSurface8 *sfcMap, int32_t iMapX, int32_t iMapY
 
 CSurface8 *C4Landscape::CreateMap()
 {
-	CSurface8 *sfcMap;
-	int32_t iWidth = 0, iHeight = 0;
+	int32_t width{0};
+	int32_t height{0};
 
 	// Create map surface
-	Game.C4S.Landscape.GetMapSize(iWidth, iHeight, Game.Parameters.StartupPlayerCount);
-	if (!(sfcMap = new CSurface8(iWidth, iHeight)))
-		return nullptr;
+	Game.C4S.Landscape.GetMapSize(width, height, Game.Parameters.StartupPlayerCount);
+	auto surfaceMap = std::make_unique<CSurface8>(width, height);
 
 	// Fill sfcMap
 	C4MapCreator MapCreator;
-	MapCreator.Create(sfcMap,
+	MapCreator.Create(surfaceMap.get(),
 		Game.C4S.Landscape, Game.TextureMap,
 		true, Game.Parameters.StartupPlayerCount);
 
-	return sfcMap;
+	return surfaceMap.release();
 }
 
 CSurface8 *C4Landscape::CreateMapS2(C4Group &ScenFile)

src/C4ObjectList.cpp
@@ -107,7 +107,6 @@ int C4ObjectList::ListIDCount(int32_t dwCategory)
 
 bool C4ObjectList::Add(C4Object *nObj, SortType eSort, C4ObjectList *pLstSorted)
 {
-	C4ObjectLink *nLnk;
 	if (!nObj || !nObj->Def || !nObj->Status) return false;
 
 #ifdef _DEBUG
@@ -126,9 +125,9 @@ bool C4ObjectList::Add(C4Object *nObj, SortType eSort, C4ObjectList *pLstSorted)
 	assert(pLstSorted != this);
 
 	// Allocate new link
-	if (!(nLnk = new C4ObjectLink)) return false;
+	auto newLink = std::make_unique<C4ObjectLink>();
 	// Set link
-	nLnk->Obj = nObj;
+	newLink->Obj = nObj;
 
 	// Search insert position (default: end of list)
 	C4ObjectLink *cLnk = nullptr, *cPrev = Last;
@@ -217,7 +216,8 @@ bool C4ObjectList::Add(C4Object *nObj, SortType eSort, C4ObjectList *pLstSorted)
 	assert(!cLnk || cLnk->Prev == cPrev);
 
 	// Insert new link after predecessor
-	InsertLink(nLnk, cPrev);
+	InsertLink(newLink.get(), cPrev);
+	newLink.release();
 
 #ifdef _DEBUG
 	// Debug: Check sort

src/C4PXS.cpp
@@ -182,7 +182,7 @@ C4PXS *C4PXSSystem::New()
 		// Create new chunk if necessary
 		if (!Chunk[cnt])
 		{
-			if (!(Chunk[cnt] = new C4PXS[PXSChunkSize])) return nullptr;
+			Chunk[cnt] = new C4PXS[PXSChunkSize];
 			iChunkPXS[cnt] = 0;
 		}
 		// Check this chunk for space
@@ -380,7 +380,7 @@ bool C4PXSSystem::Load(C4Group &hGroup)
 	if (iChunkNum > PXSMaxChunk) return false;
 	for (uint32_t cnt = 0; cnt < iChunkNum; cnt++)
 	{
-		if (!(Chunk[cnt] = new C4PXS[PXSChunkSize])) return false;
+		Chunk[cnt] = new C4PXS[PXSChunkSize];
 		if (!hGroup.Read(Chunk[cnt], iChunkSize)) return false;
 		// count the PXS, Peter!
 		// convert num format, if neccessary

src/C4PathFinder.cpp
@@ -627,18 +627,17 @@ bool C4PathFinder::AddRay(int32_t iFromX, int32_t iFromY, int32_t iToX, int32_t 
 	// Max depth
 	if (iDepth >= C4PF_MaxDepth * Level) return false;
 	// Allocate and set new ray
-	C4PathFinderRay *pRay;
-	if (!(pRay = new C4PathFinderRay)) return false;
-	pRay->X = iFromX; pRay->Y = iFromY;
-	pRay->X2 = iFromX; pRay->Y2 = iFromY;
-	pRay->TargetX = iToX; pRay->TargetY = iToY;
-	pRay->Depth = iDepth;
-	pRay->Direction = iDirection;
-	pRay->From = pFrom;
-	pRay->pPathFinder = this;
-	pRay->Next = FirstRay;
-	pRay->UseZone = pUseZone;
-	FirstRay = pRay;
+	auto ray = std::make_unique<C4PathFinderRay>();
+	ray->X = iFromX; ray->Y = iFromY;
+	ray->X2 = iFromX; ray->Y2 = iFromY;
+	ray->TargetX = iToX; ray->TargetY = iToY;
+	ray->Depth = iDepth;
+	ray->Direction = iDirection;
+	ray->From = pFrom;
+	ray->pPathFinder = this;
+	ray->Next = FirstRay;
+	ray->UseZone = pUseZone;
+	FirstRay = ray.release();
 	return true;
 }
 
@@ -647,20 +646,19 @@ bool C4PathFinder::SplitRay(C4PathFinderRay *pRay, int32_t iAtX, int32_t iAtY)
 	// Max depth
 	if (pRay->Depth >= C4PF_MaxDepth * Level) return false;
 	// Allocate and set new ray
-	C4PathFinderRay *pNewRay;
-	if (!(pNewRay = new C4PathFinderRay)) return false;
-	pNewRay->Status = C4PF_Ray_Still;
-	pNewRay->X = pRay->X; pNewRay->Y = pRay->Y;
-	pNewRay->X2 = iAtX; pNewRay->Y2 = iAtY;
-	pNewRay->TargetX = pRay->TargetX; pNewRay->TargetY = pRay->TargetY;
-	pNewRay->Depth = pRay->Depth;
-	pNewRay->Direction = pRay->Direction;
-	pNewRay->From = pRay->From;
-	pNewRay->pPathFinder = this;
-	pNewRay->Next = FirstRay;
-	FirstRay = pNewRay;
+	auto newRay = std::make_unique<C4PathFinderRay>();
+	newRay->Status = C4PF_Ray_Still;
+	newRay->X = pRay->X; newRay->Y = pRay->Y;
+	newRay->X2 = iAtX; newRay->Y2 = iAtY;
+	newRay->TargetX = pRay->TargetX; newRay->TargetY = pRay->TargetY;
+	newRay->Depth = pRay->Depth;
+	newRay->Direction = pRay->Direction;
+	newRay->From = pRay->From;
+	newRay->pPathFinder = this;
+	newRay->Next = FirstRay;
+	FirstRay = newRay.get();
 	// Adjust split ray
-	pRay->From = pNewRay;
+	pRay->From = newRay.release();
 	pRay->X = iAtX; pRay->Y = iAtY;
 	return true;
 }

commit af9014572b486aa8db928b116a504d5394de5bc7

Fix scoping mistake in C4ObjectInfoList::Load introduced in 92a5480c308095d7bf9b74ce5993d2c81e4dcb79

src/C4ObjectInfoList.cpp
@@ -68,15 +68,15 @@ int32_t C4ObjectInfoList::Load(C4Group &hGroup, bool fLoadPortraits)
 			Add(info.release());
 			++infn;
 		}
+	}
 
-		// Search subfolders
-		hGroup.ResetSearch();
-		while (hGroup.FindNextEntry("*", entryname))
-		{
-			C4Group ItemGroup;
-			if (ItemGroup.OpenAsChild(&hGroup, entryname))
-				Load(ItemGroup, fLoadPortraits);
-		}
+	// Search subfolders
+	hGroup.ResetSearch();
+	while (hGroup.FindNextEntry("*", entryname))
+	{
+		C4Group ItemGroup;
+		if (ItemGroup.OpenAsChild(&hGroup, entryname))
+			Load(ItemGroup, fLoadPortraits);
 	}
 
 	return infn;