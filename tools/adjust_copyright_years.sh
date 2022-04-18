1970531f69d6b12b8974601410c4a83474bd2171
Markus Mittendrein <github@maxmitti.tk>
2022-04-18 18:49:59 +0200

    adjust_copyright_years.sh: Add LAST_ADJUSTMENT variable to only consider changes since then
    Otherwise all files would be set to 2022, since the last change was the previous bulk-adjustment.

--- tools/adjust_copyright_years.sh
+++ tools/adjust_copyright_years.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+LAST_ADJUSTMENT=b21b64549b9d2082e18c0e0656ef2e446c27a438
+
 SCRIPT_DIR="$(realpath $(dirname $0))"
 SRC_ROOT=$SCRIPT_DIR/..
 
@@ -11,15 +13,14 @@ function check_adjust()
 	YEAR="$2"
 	if not grep -q "$YEAR, The LegacyClonk Team" "$f"; then
 		echo "Adjusting $f to $YEAR"
-		 sed -i -E -e 's/(\(c\) )([[:digit:]]{4})-([[:digit:]]{4})(, The LegacyClonk Team)/\1\2-'$YEAR'\4/' -e 's/(\(c\) )([[:digit:]]{4})(, The LegacyClonk Team)/\1\2-'$YEAR'\3/' "$f"
+		sed -i -E -e 's/(\(c\) )([[:digit:]]{4})-([[:digit:]]{4})(, The LegacyClonk Team)/\1\2-'$YEAR'\4/' -e 's/(\(c\) )([[:digit:]]{4})(, The LegacyClonk Team)/\1\2-'$YEAR'\3/' "$f"
 	fi
 }
 
 for f in "$SRC_ROOT"/src/*.* "$SRC_ROOT"/CMakeLists.txt; do
-	YEAR=$(git log -n 1 --pretty='%cd' --date='format:%Y' "$f")
+	YEAR=$(git log -n 1 --pretty='%cd' --date='format:%Y' "$LAST_ADJUSTMENT"..HEAD -- "$f")
 
 	if [ -z "$YEAR" ]; then
-		echo "Skipping $f (Unknown to git)"
 		continue
 	fi
 
