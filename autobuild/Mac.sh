35b283509e8961293bb37c426f443d952958c57c
Markus Mittendrein <github@maxmitti.tk>
2022-09-23 19:14:08 +0200

    Remove now unnecessary EXTRA_LINK_LIBS

--- autobuild/Mac.sh
+++ autobuild/Mac.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo "CMAKE_CONFIGURE_ARGS=-DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_FLAGS=-Wno-parentheses -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -C $PWD/config.txt" >> $GITHUB_ENV
-LIBS=("\"-framework "{CoreAudio,CoreVideo,AudioToolbox,AudioUnit,ForceFeedback,IOKit,Cocoa,Foundation,AVFoundation,Carbon,CoreHaptics,GameController}\")
-echo "set(EXTRA_LINK_LIBS ${LIBS[@]} CACHE STRING \"Extra libs needed for static linking\")" > config.txt
+echo "CMAKE_CONFIGURE_ARGS=-DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_FLAGS=-Wno-parentheses -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9" >> $GITHUB_ENV
 
 cat autobuild/mac_any_cast.h >> src/C4InteractiveThread.h
--- autobuild/Windows.sh
+++ autobuild/Windows.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "CMAKE_BUILD_ARGS=--config RelWithDebInfo" >> $GITHUB_ENV
 echo "CMAKE_CONFIGURE_ARGS=-DUSE_FMOD=ON" >> $GITHUB_ENV
-echo "set(EXTRA_LINK_LIBS user32 gdi32 winmm ole32 oleaut32 uuid advapi32 shell32 CACHE STRING \"Extra libs needed for static linking\")" > config.txt
