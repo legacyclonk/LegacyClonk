163f79940a8af4880bcac33163ecd6922173efb1
Jan Heberer <jangc@t-online.de>
2022-10-09 20:22:34 +0200

    Remove unused `C4ControlClientUpdate::MinReadyAnnouncementDelay`

--- src/C4Control.h
+++ src/C4Control.h
@@ -258,9 +258,6 @@ public:
 	C4ControlClientUpdate(int32_t iID, C4ControlClientUpdType eType, int32_t iData = 0)
 		: iID(iID), eType(eType), iData(iData) {}
 
-private:
-	static const int32_t MinReadyAnnouncementDelay = 1; // seconds that need to pass between ready-state announcements to prevent spam
-
 public:
 	int32_t iID;
 	C4ControlClientUpdType eType;
