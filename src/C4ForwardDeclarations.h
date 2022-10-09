7471f40f6c17ec3a35e55ece399ebc23f57ba58d
Jan Heberer <jangc@t-online.de>
2022-10-09 20:23:01 +0200

    Add file C4ForwardDeclarations.h

---
+++ src/C4ForwardDeclarations.h
@@ -0,0 +1,16 @@
+/*
+ * LegacyClonk
+ *
+ * Copyright (c) 2022, The LegacyClonk Team and contributors
+ *
+ * Distributed under the terms of the ISC license; see accompanying file
+ * "COPYING" for details.
+ *
+ * "Clonk" is a registered trademark of Matthes Bender, used with permission.
+ * See accompanying file "TRADEMARK" for details.
+ *
+ * To redistribute this file separately, substitute the full license texts
+ * for the above references.
+ */
+
+#pragma once
