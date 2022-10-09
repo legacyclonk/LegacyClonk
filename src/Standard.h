3a8a790af0fdabc81fb3a125f146dcf798416b6f
Jan Heberer <jangc@t-online.de>
2022-10-09 20:24:44 +0200

    Move `checked_cast` from Standard.h to StdHelpers.h

--- src/Standard.h
+++ src/Standard.h
@@ -24,46 +24,10 @@
 #include "C4Strings.h"
 #include "StdColors.h"
 #include "StdFile.h"
+#include "StdHelpers.h"
 
 // These functions have to be provided by the application.
 bool Log(const char *szMessage);
 bool LogSilent(const char *szMessage);
 
 #include <algorithm>
-#include <concepts>
-#include <limits>
-#include <stdexcept>
-#include <type_traits>
-#include <utility>
-
-template<std::integral To, std::integral From>
-To checked_cast(From from)
-{
-	if constexpr (std::is_signed_v<From>)
-	{
-		if constexpr (std::is_unsigned_v<To>)
-		{
-			if (from < 0)
-			{
-				throw std::runtime_error{"Conversion of negative value to unsigned type requested"};
-			}
-		}
-		else if constexpr (std::numeric_limits<From>::min() < std::numeric_limits<To>::min())
-		{
-			if (from < std::numeric_limits<To>::min())
-			{
-				throw std::runtime_error{"Conversion of value requested that is smaller than the target-type minimum"};
-			}
-		}
-	}
-
-	if constexpr (std::numeric_limits<From>::max() > std::numeric_limits<To>::max())
-	{
-		if (std::cmp_greater(from, std::numeric_limits<To>::max()))
-		{
-			throw std::runtime_error{"Conversion of value requested that is bigger than the target-type maximum"};
-		}
-	}
-
-	return static_cast<To>(from);
-}
--- src/StdHelpers.h
+++ src/StdHelpers.h
@@ -1,7 +1,7 @@
 /*
  * LegacyClonk
  *
- * Copyright (c) 2020, The LegacyClonk Team and contributors
+ * Copyright (c) 2020-2021, The LegacyClonk Team and contributors
  *
  * Distributed under the terms of the ISC license; see accompanying file
  * "COPYING" for details.
@@ -14,6 +14,44 @@
  */
 
 #pragma once
+
+#include <concepts>
+#include <limits>
+#include <stdexcept>
+#include <type_traits>
+#include <utility>
+
+template<std::integral To, std::integral From>
+To checked_cast(From from)
+{
+	if constexpr (std::is_signed_v<From>)
+	{
+		if constexpr (std::is_unsigned_v<To>)
+		{
+			if (from < 0)
+			{
+				throw std::runtime_error{"Conversion of negative value to unsigned type requested"};
+			}
+		}
+		else if constexpr (std::numeric_limits<From>::min() < std::numeric_limits<To>::min())
+		{
+			if (from < std::numeric_limits<To>::min())
+			{
+				throw std::runtime_error{"Conversion of value requested that is smaller than the target-type minimum"};
+			}
+		}
+	}
+
+	if constexpr (std::numeric_limits<From>::max() > std::numeric_limits<To>::max())
+	{
+		if (std::cmp_greater(from, std::numeric_limits<To>::max()))
+		{
+			throw std::runtime_error{"Conversion of value requested that is bigger than the target-type maximum"};
+		}
+	}
+
+	return static_cast<To>(from);
+}
 
 template<typename... T>
 class StdOverloadedCallable : public T...
