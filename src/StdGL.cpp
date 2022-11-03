bf718cb647d94a1c0aac06fb579e24afdaae7e7c
George Tokmaji <tokmajigeorge@gmail.com>
2022-11-03 12:10:59 +0100

    Use GL_NEAREST for the gamma texture

--- src/StdGL.cpp
+++ src/StdGL.cpp
@@ -1200,8 +1200,8 @@ bool CStdGL::RestoreDeviceObjects()
 				GammaTexture = {Gamma.GetSize(), 3, GL_R16, GL_RED, GL_UNSIGNED_SHORT};
 				GammaTexture.Bind(GL_TEXTURE_2D, 3);
 
-				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
