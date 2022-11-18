4ba469840d2f71c282eb5c9e7254b72f413c3690
Jan Heberer <jangc@t-online.de>
2022-11-18 21:05:57 +0100

    C4Shape: move class C4Rect to separate file

--- src/C4Shape.h
+++ src/C4Shape.h
@@ -14,93 +14,17 @@
  * for the above references.
  */
 
-/* Basic classes for rectangles and vertex outlines */
+/* Basic class for vertex outlines */
 
 #pragma once
 
 #include "C4Constants.h"
 #include "C4ForwardDeclarations.h"
+#include "C4Rect.h"
 
-#include <cstddef>
 #include <cstdint>
-#include <vector>
 
 #define C4D_VertexCpyPos (C4D_MaxVertex/2)
-
-class C4Rect
-{
-public:
-	int32_t x, y, Wdt, Hgt;
-
-public:
-	void Set(int32_t iX, int32_t iY, int32_t iWdt, int32_t iHgt);
-	void Default();
-	bool Overlap(C4Rect &rTarget);
-	void Intersect(const C4Rect &r2);
-	void Add(const C4Rect &r2);
-	bool operator==(const C4Rect &r2) const { return !((x - r2.x) | (y - r2.y) | (Wdt - r2.Wdt) | (Hgt - r2.Hgt)); }
-
-	bool Contains(int32_t iX, int32_t iY)
-	{
-		return iX >= x && iX < x + Wdt && iY >= y && iY < y + Hgt;
-	}
-
-	bool Contains(int32_t iX, int32_t iY, int32_t iWdt, int32_t iHgt)
-	{
-		return iX >= x && iX + iWdt < x + Wdt && iY >= y && iY + iHgt < y + Hgt;
-	}
-
-	bool Contains(const C4Rect &rect)
-	{
-		return Contains(rect.x, rect.y, rect.Wdt, rect.Hgt);
-	}
-
-	bool IntersectsLine(int32_t iX, int32_t iY, int32_t iX2, int32_t iY2);
-
-	void Normalize()
-	{
-		if (Wdt < 0) { x += Wdt + 1; Wdt = -Wdt; } if (Hgt < 0) { y += Hgt + 1; Hgt = -Hgt; }
-	}
-
-	void Enlarge(int32_t iBy)
-	{
-		x -= iBy; y -= iBy; Wdt += 2 * iBy; Hgt += 2 * iBy;
-	}
-
-	int32_t GetMiddleX() { return x + Wdt / 2; }
-	int32_t GetMiddleY() { return y + Hgt / 2; }
-	int32_t GetBottom() { return y + Hgt; }
-
-	C4Rect(int32_t tx, int32_t ty, int32_t twdt, int32_t thgt)
-	{
-		x = tx; y = ty; Wdt = twdt; Hgt = thgt;
-	}
-
-	C4Rect() {} // default ctor; doesn't initialize
-
-	C4Rect Scaled(float scale) const noexcept;
-
-	void CompileFunc(StdCompiler *pComp);
-};
-
-class C4TargetRect : public C4Rect
-{
-public:
-	int32_t tx, ty;
-
-public:
-	C4TargetRect(int32_t iX, int32_t iY, int32_t iWdt, int32_t iHgt, int32_t iTX, int32_t iTY)
-		: C4Rect(iX, iY, iWdt, iHgt), tx(iTX), ty(iTY) {}
-	C4TargetRect() {} // default ctor; doesn't initialize
-	void Set(int32_t iX, int32_t iY, int32_t iWdt, int32_t iHgt, int32_t iTX, int32_t iTY);
-	void Default();
-	bool ClipBy(C4TargetRect &rClip); // clip this rectangle by the given one (adding target positions) - return false if they don't overlap
-
-	void CompileFunc(StdCompiler *pComp);
-};
-
-const C4Rect Rect0(0, 0, 0, 0);
-const C4TargetRect TargetRect0(0, 0, 0, 0, 0, 0);
 
 // a functional class to provide density for coordinates
 class C4DensityProvider
@@ -151,26 +75,4 @@ public:
 	int32_t GetVertexContact(int32_t iVtx, uint32_t dwCheckMask, int32_t tx, int32_t ty, const C4DensityProvider &rDensityProvider = DefaultDensityProvider); // get CNAT-mask for given vertex - does not check range for iVtx!
 	void CreateOwnOriginalCopy(C4Shape &rFrom); // create copy of all vertex members in back area of own buffers
 	void CompileFunc(StdCompiler *pComp, bool fRuntime);
-};
-
-// a bunch of rectangles
-// rects NOT including pos+size-point
-class C4RectList : public std::vector<C4Rect>
-{
-public:
-	void AddRect(const C4Rect &rNewRect)
-	{
-		push_back(rNewRect);
-	}
-
-	void RemoveIndexedRect(size_t idx)
-	{
-		if (idx < GetCount() - 1) Get(idx) = Get(GetCount() - 1); pop_back();
-	}
-
-	void Clear() { clear(); }
-	size_t GetCount() const { return size(); }
-	C4Rect &Get(size_t idx) { return (*this)[idx]; } // access w/o range check
-
-	void ClipByRect(const C4Rect &rClip); // split up rectangles
 };
