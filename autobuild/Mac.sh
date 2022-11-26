d1e75fddcfe54dbf6f18c613b7fe3ab10bb2ac47
George Tokmaji <tokmajigeorge@gmail.com>
2022-11-27 00:09:18 +0100

    Mac: Use LLVM 15 from Homebrew

--- autobuild/Mac.sh
+++ autobuild/Mac.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo "CMAKE_CONFIGURE_ARGS=-DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_FLAGS=-Wno-parentheses -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9" >> $GITHUB_ENV
+brew install llvm
+echo "CMAKE_CONFIGURE_ARGS=-DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=/usr/local/opt/llvm/bin/clang -DCMAKE_CXX_COMPILER=/usr/local/opt/llvm/bin/clang++ -DCMAKE_CXX_FLAGS=-Wno-parentheses -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9" >> $GITHUB_ENV
 
 cat autobuild/mac_any_cast.h >> src/C4InteractiveThread.h
