940d7c233f29683235dc6b3f5e502eb7b703a1ec
George Tokmaji <tokmajigeorge@gmail.com>
2023-03-29 19:34:43 +0200

    macOS: Use LLVM 16

--- autobuild/Mac.sh
+++ autobuild/Mac.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-brew install llvm
+brew update
+brew install llvm@16
 echo "CMAKE_CONFIGURE_ARGS=-DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=/usr/local/opt/llvm/bin/clang -DCMAKE_CXX_COMPILER=/usr/local/opt/llvm/bin/clang++ -DCMAKE_CXX_FLAGS=-Wno-parentheses -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9" >> $GITHUB_ENV
 
 cat autobuild/mac_any_cast.h >> src/C4InteractiveThread.h
