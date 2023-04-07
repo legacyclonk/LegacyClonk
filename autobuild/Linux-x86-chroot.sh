3848765ca74984b86caef1bc7fc91da655c06f02
George Tokmaji <tokmajigeorge@gmail.com>
2023-04-07 15:29:17 +0200

    Remove Linux x86 autobuild

--- autobuild/Linux-x86-chroot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export MACHINE=i686
-ARGS=("cd $PWD;" "$@")
-eval $(echo sudo -E chroot chroot bash -c "'${ARGS[@]}'")
--- autobuild/Linux-x86.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "CHROOT=$PWD/autobuild/Linux-x86-chroot.sh" >> $GITHUB_ENV
