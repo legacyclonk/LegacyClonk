2b657eb50befeeb21fc926110a01c88c5907fe78
George Tokmaji <tokmajigeorge@gmail.com>
2023-04-09 20:38:50 +0200

    Mac: Enable -fexperimental-library for <format> and use libc++

---
+++ autobuild/clang16_mac.cmake
@@ -0,0 +1,9 @@
+cmake_minimum_required(VERSION 3.20)
+set(CMAKE_C_COMPILER "/usr/local/opt/llvm/bin/clang")
+set(CMAKE_CXX_COMPILER "/usr/local/opt/llvm/bin/clang++")
+
+set(CMAKE_C_FLAGS_INIT "-stdlib=libc++ -fexperimental-library -Wno-parentheses")
+set(CMAKE_CXX_FLAGS_INIT "-stdlib=libc++ -fexperimental-library -Wno-parentheses")
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-L/usr/local/opt/llvm/lib/c++")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "-L/usr/local/opt/llvm/lib/c++")
