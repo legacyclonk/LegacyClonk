5a55fa96ec23eea4f10887bb56d026f9b9438f26
George Tokmaji <tokmajigeorge@gmail.com>
2023-04-11 14:57:04 +0200

    CI: Disable USE_PCH with Ninja due to dependency cycles

--- autobuild/Mac.sh
+++ autobuild/Mac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 brew update
 brew install llvm@16
-echo "CMAKE_CONFIGURE_ARGS=-GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_TOOLCHAIN_FILE=$PWD/autobuild/clang16_mac.cmake" >> $GITHUB_ENV
+echo "CMAKE_CONFIGURE_ARGS=-GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_TOOLCHAIN_FILE=$PWD/autobuild/clang16_mac.cmake -DUSE_PCH=OFF" >> $GITHUB_ENV
 
 cat autobuild/mac_any_cast.h >> src/C4InteractiveThread.h
