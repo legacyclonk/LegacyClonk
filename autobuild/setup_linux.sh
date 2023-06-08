d9bbcdd6424bed133904a44af43dc2f502d34dfc
Markus Mittendrein <github@maxmitti.tk>
2023-06-08 15:30:09 +0200

    Use gcc 13.1.0 release version

--- autobuild/setup_linux.sh
+++ autobuild/setup_linux.sh
@@ -5,4 +5,6 @@ sudo update-alternatives --remove-all gcov
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends zlib1g-dev libgl1-mesa-dev libjpeg-dev libpng-dev libssl-dev libglu1-mesa-dev libglew-dev libsdl2-dev libsdl2-mixer-dev libfreetype6-dev freeglut3-dev libxpm-dev libgtk2.0-dev libnotify-dev cmake make git -y
-curl -L https://crsm.cf/pages/files/gcc-13_20.04.tar.zst | sudo tar -x --zstd -C /
+
+GCC_VERSION=13.1.0
+curl -L "https://github.com/legacyclonk/gcc-build/releases/download/$GCC_VERSION/gcc-$GCC_VERSION.tar.zst" | sudo tar -x --zstd -C /
