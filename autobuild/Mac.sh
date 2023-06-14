b19415d13d80001a2edd134a4fe5bbb734b38afe
George Tokmaji <tokmajigeorge@gmail.com>
2023-06-14 20:03:53 +0200

    macOS: Disable Homebrew install cleanup in autobuilds since it takes forever

--- autobuild/Mac.sh
+++ autobuild/Mac.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export HOMEBREW_NO_INSTALL_CLEANUP=1
 brew update
 brew install llvm@16 ninja
 echo "CMAKE_CONFIGURE_ARGS=-DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_TOOLCHAIN_FILE=$PWD/autobuild/clang16_mac.cmake" >> $GITHUB_ENV
