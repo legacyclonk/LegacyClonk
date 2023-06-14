2e4dd7330f47b4e3926656c7d55b83a3c421434a
George Tokmaji <tokmajigeorge@gmail.com>
2023-06-14 20:24:05 +0200

    macOS: Also set HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK in the autobuild for faster builds

--- autobuild/Mac.sh
+++ autobuild/Mac.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export HOMEBREW_NO_INSTALL_CLEANUP=1
+export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
 brew update
 brew install llvm@16 ninja
 echo "CMAKE_CONFIGURE_ARGS=-DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_TOOLCHAIN_FILE=$PWD/autobuild/clang16_mac.cmake" >> $GITHUB_ENV
