6e3094040960fd736ff519383bff576d02b3379f
George Tokmaji <tokmajigeorge@gmail.com>
2023-08-09 12:50:04 +0200

    Add support for tests using Boost.Test

---
+++ tests/BoostTest.h
@@ -0,0 +1,26 @@
+/*
+ * LegacyClonk
+ *
+ * Copyright (c) 2023, The LegacyClonk Team and contributors
+ *
+ * Distributed under the terms of the ISC license; see accompanying file
+ * "COPYING" for details.
+ *
+ * "Clonk" is a registered trademark of Matthes Bender, used with permission.
+ * See accompanying file "TRADEMARK" for details.
+ *
+ * To redistribute this file separately, substitute the full license texts
+ * for the above references.
+ */
+
+#pragma once
+
+#ifdef _MSC_VER
+#pragma warning(push, 1)
+#endif
+
+#include <boost/test/included/unit_test.hpp>
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
