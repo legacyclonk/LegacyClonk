40c05cbd2e70cfb60d7ca54bb7c781de94d4d1f0
George Tokmaji <tokmajigeorge@gmail.com>
2023-09-19 19:58:54 +0200

    Mac: Set CMAKE_OBJCXX_STANDARD and CMAKE_OBJCXX_FLAGS_INIT to the same values as their CXX counterparts

--- autobuild/clang16_mac.cmake
+++ autobuild/clang16_mac.cmake
@@ -3,7 +3,8 @@ set(CMAKE_C_COMPILER "/usr/local/opt/llvm/bin/clang")
 set(CMAKE_CXX_COMPILER "/usr/local/opt/llvm/bin/clang++")
 
 set(CMAKE_C_FLAGS_INIT "-stdlib=libc++ -fexperimental-library -Wno-parentheses")
-set(CMAKE_CXX_FLAGS_INIT "-stdlib=libc++ -fexperimental-library -Wno-parentheses")
+set(CMAKE_CXX_FLAGS_INIT "${CMAKE_C_FLAGS_INIT}")
+set(CMAKE_OBJCXX_FLAGS_INIT "${CMAKE_CXX_FLAGS_INIT}")
 
 set(CMAKE_EXE_LINKER_FLAGS_INIT "-L/usr/local/opt/llvm/lib/c++")
 set(CMAKE_SHARED_LINKER_FLAGS_INIT "-L/usr/local/opt/llvm/lib/c++")
