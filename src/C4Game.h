f35a2a1cd47e7eead1a95ee80d132f989c0fb468
George Tokmaji <tokmajigeorge@gmail.com>
2023-11-17 15:06:57 +0100

    Refactor section initialization sequence, make FnCreateSection be able to create a section with an empty landscape from a map providing C4SLandscape initialization data

--- src/C4Game.h
+++ src/C4Game.h
@@ -312,6 +312,7 @@ public:
 	bool InitKeyboard(); // register main keyboard input functions
 
 	std::int32_t CreateSection(const char *name);
+	std::int32_t CreateEmptySection(const char *name, const C4SLandscape &landscape);
 
 protected:
 	bool InitSystem();
