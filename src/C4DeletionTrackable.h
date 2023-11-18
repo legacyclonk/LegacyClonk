2e0fbda7065c3cb3e2ca5f7acb63cdb2bcdb9d7e
Markus Mittendrein <git@maxmitti.tk>
2023-11-18 23:12:50 +0100

    Add C4DeletionTrackable

---
+++ src/C4DeletionTrackable.h
@@ -0,0 +1,104 @@
+/*
+ * LegacyClonk
+ *
+ * Copyright (c) 2023, The LegacyClonk Team and contributors
+ *
+ * Distributed under the terms of the ISC license; see accompanying file
+ * "COPYING" for details.
+ *
+ * "Clonk" is a registered trademark of Matthes Bender, used with permission.
+ * See accompanying file "TRADEMARK" for details.
+ *
+ * To redistribute this file separately, substitute the full license texts
+ * for the above references.
+ */
+
+#pragma once
+
+#include <cassert>
+#include <utility>
+
+class C4DeletionTrackable
+{
+	class Tracker
+	{
+	public:
+		constexpr Tracker(C4DeletionTrackable *tracked) noexcept : tracked{tracked}
+		{
+			assert(!tracked->tracker);
+			tracked->tracker = this;
+		}
+
+		Tracker(const Tracker &) = delete;
+
+		constexpr Tracker(Tracker &&other) noexcept : tracked{std::exchange(other.tracked, nullptr)}, isDeleted{other.isDeleted}
+		{
+			if (!tracked)
+			{
+				return;
+			}
+
+			assert(tracked->tracker == &other);
+			tracked->tracker = this;
+		}
+
+		Tracker &operator=(const Tracker &) = delete;
+
+		constexpr Tracker &operator=(Tracker &&other) noexcept
+		{
+			clear();
+			tracked = std::exchange(other.tracked, nullptr);
+			isDeleted = other.isDeleted;
+
+			if (tracked)
+			{
+				assert(tracked->tracker == &other);
+				tracked->tracker = this;
+			}
+
+			return *this;
+		}
+
+		constexpr ~Tracker() noexcept
+		{
+			clear();
+		}
+
+		constexpr bool IsDeleted() const noexcept
+		{
+			return isDeleted;
+		}
+
+	private:
+		constexpr void clear() noexcept
+		{
+			if (tracked && !isDeleted)
+			{
+				assert(tracked->tracker == this);
+				tracked->tracker = nullptr;
+			}
+		}
+
+		C4DeletionTrackable *tracked{nullptr};
+		bool isDeleted{false};
+
+		friend C4DeletionTrackable;
+	};
+
+public:
+	constexpr ~C4DeletionTrackable() noexcept
+	{
+		if (tracker)
+		{
+			tracker->isDeleted = true;
+		}
+	}
+
+	[[nodiscard]] constexpr Tracker TrackDeletion() noexcept
+	{
+		return {this};
+	}
+
+private:
+	Tracker *tracker{nullptr};
+};
