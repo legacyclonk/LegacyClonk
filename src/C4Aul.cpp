b84cb79b36327a19981ba2ecf55144303d24eb51
Markus Mittendrein <github@maxmitti.tk>
2023-12-02 18:56:30 +0100

    C4AulScript: Replace Code and related members with std::vector

--- src/C4Aul.cpp
+++ src/C4Aul.cpp
@@ -178,8 +178,7 @@ void C4AulScript::Default()
 	// not compiled
 	State = ASS_NONE;
 	Script.Clear();
-	Code = CPos = nullptr;
-	CodeSize = CodeBufSize = 0;
+	Code.clear();
 	IncludesResolved = false;
 
 	// defaults
@@ -225,8 +224,7 @@ void C4AulScript::Clear()
 	while (Func0) delete Func0;
 	// delete script+code
 	Script.Clear();
-	delete[] Code; Code = nullptr;
-	CodeSize = CodeBufSize = 0;
+	Code.clear();
 	// reset flags
 	State = ASS_NONE;
 }
