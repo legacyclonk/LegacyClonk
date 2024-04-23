37ac40b71359ca174b273acca8ba96016446cf3e
George Tokmaji <tokmajigeorge@gmail.com>
2024-04-23 08:18:20 +0200

    C4Object: Fix resorting being broken due to the wrong flag being used

--- src/C4Game.cpp
+++ src/C4Game.cpp
@@ -1118,7 +1118,6 @@ void C4Game::Default()
 	pGUI = nullptr;
 	pScenarioSections = pCurrentScenarioSection = nullptr;
 	*CurrentScenarioSection = 0;
-	fResortAnyObject = false;
 	pNetworkStatistics = nullptr;
 	IsMusicEnabled = false;
 	iMusicLevel = 100;
@@ -1254,7 +1253,6 @@ void C4Game::CompileFunc(StdCompiler *pComp, CompileSettings comp)
 		pComp->Value(mkNamingAdapt(Rules,                                   "Rules",                  0));
 		pComp->Value(mkNamingAdapt(PlayList,                                "PlayList",               ""));
 		pComp->Value(mkNamingAdapt(mkStringAdaptMA(CurrentScenarioSection), "CurrentScenarioSection", ""));
-		pComp->Value(mkNamingAdapt(fResortAnyObject,                        "ResortAnyObj",           false));
 		pComp->Value(mkNamingAdapt(IsMusicEnabled,                          "MusicEnabled",           false));
 		pComp->Value(mkNamingAdapt(iMusicLevel,                             "MusicLevel",             100));
 		pComp->Value(mkNamingAdapt(NextMission,                             "NextMission",            StdStrBuf()));
