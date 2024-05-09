95e7267d4c76abaee85974cccdfdca5e651e457b
George Tokmaji <tokmajigeorge@gmail.com>
2024-05-09 13:13:55 +0200

    Linux: Use GCC 14

--- autobuild/gcc14_20.04.cmake
+++ autobuild/gcc14_20.04.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.20)
-set(CMAKE_C_COMPILER x86_64-linux-gnu-x86_64-linux-gnu-gcc-13)
-set(CMAKE_CXX_COMPILER x86_64-linux-gnu-x86_64-linux-gnu-g++-13)
+set(CMAKE_C_COMPILER x86_64-linux-gnu-x86_64-linux-gnu-gcc-14)
+set(CMAKE_CXX_COMPILER x86_64-linux-gnu-x86_64-linux-gnu-g++-14)
 
 set(CMAKE_C_FLAGS_INIT "-static-libgcc")
 set(CMAKE_CXX_FLAGS_INIT "-static-libgcc -static-libstdc++")
