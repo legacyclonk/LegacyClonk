bfb0ce01a9ee83dcd4265346c01afc8caf5652c8
George Tokmaji <tokmajigeorge@gmail.com>
2024-05-24 20:09:46 +0200

    C4Awaiter: Unify ResumeOnSignal and ResumeOnSignals and fix a race condition in setting the cancellation event

--- src/C4Awaiter.h
+++ src/C4Awaiter.h
@@ -57,99 +57,42 @@ namespace C4Awaiter
 		};
 
 #ifndef _WIN32
-		class ResumeOnSignal : public C4Task::CancellableAwaiter<ResumeOnSignal>
+		template<bool Single>
+		class ResumeOnSignals : public C4Task::CancellableAwaiter<ResumeOnSignals<Single>>
 		{
-		public:
-			ResumeOnSignal(const pollfd fd, const std::uint32_t timeout) : fds{{fd, {.fd = -1}}}, timeout{timeout} {}
-
-		public:
-			bool await_ready() noexcept
-			{
-				return StdSync::Poll(std::span{fds.begin(), 1}, 0) == 1;
-			}
-
-			template<typename T>
-			void await_suspend(const std::coroutine_handle<T> handle)
-			{
-				const std::size_t size{fds.size()};
-				SetCancellablePromise(handle);
-
-				C4ThreadPool::Global->SubmitCallback([handle, size, this]
-				{
-					const struct Cleanup
-					{
-						~Cleanup()
-						{
-							handle.resume();
-						}
-
-						std::coroutine_handle<> handle;
-					} cleanup{handle};
-
-					try
-					{
-						switch (StdSync::Poll(fds, timeout))
-						{
-						case -1:
-							throw std::runtime_error{std::string{"poll failed: "} + std::strerror(errno)};
-
-						case 0:
-							return;
-
-						default:
-							if (cancellationEvent && fds[fds.size() - 1].revents & POLLIN)
-							{
-								throw C4Task::CancelledException{};
-							}
-
-							return;
-						}
-					}
-					catch (...)
-					{
-						exception = std::current_exception();
-					}
-				});
-			}
-
-			short await_resume() const
-			{
-				if (exception)
-				{
-					std::rethrow_exception(exception);
-				}
-
-				return fds[0].revents;
-			}
-
-			void SetupCancellation(C4Task::CancellablePromise *const promise)
-			{
-				cancellationEvent.emplace();
-				fds[1].fd = cancellationEvent->GetFD();
-
-				promise->SetCancellationCallback([](void *const argument)
-				{
-					reinterpret_cast<CStdEvent *>(argument)->Set();
-				}, &*cancellationEvent);
-			}
-
 		private:
-			std::array<pollfd, 2> fds;
-			std::uint32_t timeout;
-			std::exception_ptr exception;
-			std::optional<CStdEvent> cancellationEvent;
-		};
+			enum class State
+			{
+				Idle,
+				Pending,
+				Cancelling,
+				Cancelled
+			};
 
-		class ResumeOnSignals : public C4Task::CancellableAwaiter<ResumeOnSignals>
-		{
+			using PollFdContainer = std::conditional_t<Single, std::array<pollfd, 2>, std::vector<pollfd>>;
+
 		public:
 			template<std::ranges::range T>
-			ResumeOnSignals(T &&range, const std::uint32_t timeout) : fds{std::ranges::begin(range), std::ranges::end(range)}, timeout{timeout} {}
+			ResumeOnSignals(T &&range, const std::uint32_t timeout) requires (!Single) : fds{std::ranges::begin(range), std::ranges::end(range)}, timeout{timeout}
+			{
+				fds.push_back(pollfd{.fd = cancellationEvent.GetFD(), .events = POLLIN});
+			}
+
+			ResumeOnSignals(const pollfd fd, const std::uint32_t timeout) requires Single
+				: fds{{fd, pollfd{.fd = cancellationEvent.GetFD(), .events = POLLIN}}},
+				  timeout{timeout}
+			{
+			}
+
+			~ResumeOnSignals()
+			{
+				assert(state.load(std::memory_order_acquire) == State::Idle);
+			}
 
 		public:
 			bool await_ready() noexcept
 			{
-				return StdSync::Poll(fds, 0) > 0;
+				return StdSync::Poll(std::span{fds.data(), fds.size() - 1}, 0) > 0;
 			}
 
 			template<typename T>
@@ -157,7 +100,13 @@ namespace C4Awaiter
 			{
 				const std::size_t size{fds.size()};
 
-				SetCancellablePromise(handle);
+				C4Task::CancellableAwaiter<ResumeOnSignals<Single>>::SetCancellablePromise(handle);
+
+				State expected{State::Idle};
+				if (!state.compare_exchange_strong(expected, State::Pending, std::memory_order_acq_rel))
+				{
+					handle.resume();
+				}
 
 				C4ThreadPool::Global->SubmitCallback([handle, size, this]
 				{
@@ -182,11 +131,6 @@ namespace C4Awaiter
 							return;
 
 						default:
-							if (cancellationEvent && fds[fds.size() - 1].revents & POLLIN)
-							{
-								throw C4Task::CancelledException{};
-							}
-
 							return;
 						}
 					}
@@ -197,39 +141,68 @@ namespace C4Awaiter
 				});
 			}
 
-			std::vector<pollfd> await_resume()
+			std::vector<pollfd> await_resume() requires (!Single)
 			{
-				if (exception)
-				{
-					std::rethrow_exception(exception);
-				}
+				CheckCancellationAndException();
 
-				if (cancellationEvent)
-				{
-					fds.resize(fds.size() - 1);
-				}
+				fds.resize(fds.size() - 1);
 
 				std::erase_if(fds, [](const auto &fd) { return fd.revents == 0; });
 
 				return std::move(fds);
 			}
 
+			pollfd await_resume() requires Single
+			{
+				CheckCancellationAndException();
+
+				return fds[0];
+			}
+
 			void SetupCancellation(C4Task::CancellablePromise *const promise)
 			{
-				cancellationEvent.emplace();
-				fds.push_back({.fd = cancellationEvent->GetFD(), .events = POLLIN});
-
 				promise->SetCancellationCallback([](void *const argument)
 				{
-					reinterpret_cast<CStdEvent *>(argument)->Set();
-				}, &*cancellationEvent);
+					auto &awaiter = *reinterpret_cast<ResumeOnSignals *>(argument);
+					State expected{State::Pending};
+					if (awaiter.state.compare_exchange_strong(expected, State::Cancelling, std::memory_order_acq_rel))
+					{
+						awaiter.cancellationEvent.Set();
+						awaiter.state.store(State::Cancelled, std::memory_order_release);
+					}
+				}, this);
 			}
 
 		private:
-			std::vector<pollfd> fds;
+			void CheckCancellationAndException()
+			{
+				State expected{State::Pending};
+
+				while (!state.compare_exchange_weak(expected, State::Idle, std::memory_order_acq_rel))
+				{
+					if (expected == State::Cancelling)
+					{
+						expected = State::Cancelled;
+					}
+				}
+
+				if (expected == State::Cancelled)
+				{
+					throw C4Task::CancelledException{};
+				}
+
+				if (exception)
+				{
+					std::rethrow_exception(exception);
+				}
+			}
+
+		private:
+			CStdEvent cancellationEvent;
+			PollFdContainer fds;
 			std::uint32_t timeout;
+			std::atomic<State> state{State::Idle};
 			std::exception_ptr exception;
-			std::optional<CStdEvent> cancellationEvent;
 		};
 #endif
 	}
@@ -257,13 +230,13 @@ namespace C4Awaiter
 		}
 	}
 #else
-	[[nodiscard]] inline Awaiter::ResumeOnSignal ResumeOnSignal(const pollfd fd, const std::uint32_t timeout = StdSync::Infinite) noexcept
+	[[nodiscard]] inline Awaiter::ResumeOnSignals<true> ResumeOnSignal(const pollfd fd, const std::uint32_t timeout = StdSync::Infinite) noexcept
 	{
 		return {fd, timeout};
 	}
 
 	template<std::ranges::range T>
-	[[nodiscard]] inline Awaiter::ResumeOnSignals ResumeOnSignals(T &&fds, const std::uint32_t timeout = StdSync::Infinite) noexcept
+	[[nodiscard]] inline Awaiter::ResumeOnSignals<false> ResumeOnSignals(T &&fds, const std::uint32_t timeout = StdSync::Infinite) noexcept
 	{
 		return {std::forward<T>(fds), timeout};
 	}
