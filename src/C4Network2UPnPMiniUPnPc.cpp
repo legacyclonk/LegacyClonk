fbfa7a55bc638b2c8126a25d55977b25f56c1577
George Tokmaji <tokmajigeorge@gmail.com>
2024-07-24 22:53:34 +0200

    Adjust copyright notices to indicate that the code was partially backported from OpenClonk

--- src/C4Network2UPnPMiniUPnPc.cpp
+++ src/C4Network2UPnPMiniUPnPc.cpp
@@ -1,7 +1,8 @@
 /*
  * LegacyClonk
  *
- * Copyright (c) 2022, The LegacyClonk Team and contributors
+ * Copyright (c) 2012-2016, The OpenClonk Team and contributors
+ * Copyright (c) 2024, The LegacyClonk Team and contributors
  *
  * Distributed under the terms of the ISC license; see accompanying file
  * "COPYING" for details.
