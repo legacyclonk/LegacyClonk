20cabdbb558ed812a9e61a2911b42467967aab72
George Tokmaji <tokmajigeorge@gmail.com>
2024-11-24 21:00:41 +0100

    Bump version to 4.9.11.0

--- src/C4Version.h
+++ src/C4Version.h
@@ -27,8 +27,8 @@
 /* These values are now controlled by the file source/version - DO NOT MODIFY DIRECTLY */
 #define C4XVER1 4
 #define C4XVER2 9
-#define C4XVER3 10
-#define C4XVER4 15
+#define C4XVER3 11
+#define C4XVER4 0
 #define C4XVERBUILD 360
 #define C4VERSIONEXTRA ""
 /* These values are now controlled by the file source/version - DO NOT MODIFY DIRECTLY */
