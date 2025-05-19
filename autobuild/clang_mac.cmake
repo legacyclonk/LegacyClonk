7f4cc847de6303d1b4e1c8c0416b4cc906f4f926
Markus Mittendrein <git@maxmitti.at>
2025-05-19 16:02:57 +0200

    CI: Clang Mac: Use libc++ (specified differently)

--- autobuild/clang_mac.cmake
+++ autobuild/clang_mac.cmake
@@ -3,8 +3,8 @@ set(CMAKE_C_COMPILER "/usr/local/opt/llvm/bin/clang")
 set(CMAKE_CXX_COMPILER "/usr/local/opt/llvm/bin/clang++")
 
 set(CMAKE_C_FLAGS_INIT "-fexperimental-library -Wno-parentheses")
-set(CMAKE_CXX_FLAGS_INIT "-nostdlib++ ${CMAKE_C_FLAGS_INIT}")
-set(CMAKE_OBJCXX_FLAGS_INIT "-nostdlib++ ${CMAKE_CXX_FLAGS_INIT}")
+set(CMAKE_CXX_FLAGS_INIT "-stdlib=libc++ -static-libstdc++ ${CMAKE_C_FLAGS_INIT}")
+set(CMAKE_OBJCXX_FLAGS_INIT "-stdlib=libc++ -static-libstdc++ ${CMAKE_CXX_FLAGS_INIT}")
 
-set(CMAKE_EXE_LINKER_FLAGS_INIT "/usr/local/opt/llvm/lib/c++/libc++.a /usr/local/opt/llvm/lib/c++/libc++abi.a /usr/local/opt/llvm/lib/c++/libc++experimental.a")
-set(CMAKE_SHARED_LINKER_FLAGS_INIT "/usr/local/opt/llvm/lib/c++/libc++.a /usr/local/opt/llvm/lib/c++/libc++abi.a /usr/local/opt/llvm/lib/c++/libc++experimental.a")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-stdlib=libc++ -static-libstdc++")
+set(CMAKE_SHARED_LINKER_FLAGS_INIT "-stdlib=libc++ -static-libstdc++")
