1d4434e9bc2dae94505b40e8cb42b1af3432c507
Markus Mittendrein <git@maxmitti.at>
2025-05-19 16:12:07 +0200

    CI: Linux: Install fuse

--- autobuild/setup_linux.sh
+++ autobuild/setup_linux.sh
@@ -4,7 +4,7 @@ sudo update-alternatives --remove-all g++
 sudo update-alternatives --remove-all gcov
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends zlib1g-dev libgl1-mesa-dev libjpeg-dev libpng-dev libssl-dev libglu1-mesa-dev libglew-dev libsdl2-dev libsdl2-mixer-dev libfreetype6-dev freeglut3-dev libxpm-dev libgtk-3-dev libnotify-dev cmake ninja-build git -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends zlib1g-dev libgl1-mesa-dev libjpeg-dev libpng-dev libssl-dev libglu1-mesa-dev libglew-dev libsdl2-dev libsdl2-mixer-dev libfreetype6-dev freeglut3-dev libxpm-dev libgtk-3-dev libnotify-dev cmake ninja-build git fuse -y
 
 GCC_VERSION=15.1.0
 curl -L "https://github.com/legacyclonk/gcc-build/releases/download/$GCC_VERSION/gcc-$GCC_VERSION.tar.zst" | sudo tar -x --zstd -C /
