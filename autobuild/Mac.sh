5f9e0f38e650c395f1d06efad659fe68000e64f8
Markus Mittendrein <github@maxmitti.tk>
2025-09-14 15:06:26 +0200

    macOS: Autobuild fixes

--- autobuild/Mac.sh
+++ autobuild/Mac.sh
@@ -6,3 +6,6 @@ brew install llvm@21 ninja
 echo "CMAKE_CONFIGURE_ARGS=-DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_TOOLCHAIN_FILE=$PWD/autobuild/clang_mac.cmake" >> $GITHUB_ENV
 
 cat autobuild/mac_any_cast.h >> src/C4InteractiveThread.h
+
+echo "target_include_directories(clonk BEFORE PUBLIC /usr/local/opt/llvm/include/c++/v1)" >> CMakeLists.txt
+cat CMakeLists.txt
