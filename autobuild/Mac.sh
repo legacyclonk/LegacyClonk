824c384e903387a7743a10b0d638f59c10eaeb9e
Markus Mittendrein <github@maxmitti.tk>
2025-09-14 15:13:14 +0200

    Try include_directories with BEFORE in CMakeLists.txt

--- autobuild/Mac.sh
+++ autobuild/Mac.sh
@@ -3,6 +3,8 @@ export HOMEBREW_NO_INSTALL_CLEANUP=1
 export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
 brew update
 brew install llvm@21 ninja
-echo "CMAKE_CONFIGURE_ARGS=-DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_TOOLCHAIN_FILE=$PWD/autobuild/clang_mac.cmake -DCMAKE_CXX_FLAGS=-I/usr/local/opt/llvm/include/c++/v1" >> $GITHUB_ENV
+echo "CMAKE_CONFIGURE_ARGS=-DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 -DCMAKE_TOOLCHAIN_FILE=$PWD/autobuild/clang_mac.cmake" >> $GITHUB_ENV
 
 cat autobuild/mac_any_cast.h >> src/C4InteractiveThread.h
+
+echo "include_directories(BEFORE /usr/local/opt/llvm/include/c++/v1)" >> CMakeLists.txt
