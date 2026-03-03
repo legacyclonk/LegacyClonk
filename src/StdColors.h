5be837e0574cf2cf68c1e56de949c99aed117b27
Markus Mittendrein <github@maxmitti.tk>
2026-03-03 22:18:06 +0100

    Update copyright years in source code

--- src/StdColors.h
+++ src/StdColors.h
@@ -4,7 +4,7 @@
  * Copyright (c) RedWolf Design
  * Copyright (c) 1998-2000, Matthes Bender (RedWolf Design)
  * Copyright (c) 2005, Sven2
- * Copyright (c) 2017-2021, The LegacyClonk Team and contributors
+ * Copyright (c) 2017-2022, The LegacyClonk Team and contributors
  *
  * Distributed under the terms of the ISC license; see accompanying file
  * "COPYING" for details.
