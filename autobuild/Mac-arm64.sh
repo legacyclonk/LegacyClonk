5fac21e9ac5f67836d4ab458274a107875ebf2dc
Simon Lauger <simon@lauger.de>
2026-04-20 13:48:22 +0200

    fix: add catch2 to ARM Mac Homebrew dependencies

--- autobuild/Mac-arm64.sh
+++ autobuild/Mac-arm64.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Install dependencies from Homebrew for ARM Mac builds,
 # since no prebuilt deps package exists for this architecture yet.
-brew install sdl2 sdl2_mixer freetype libpng jpeg-turbo glew fmt openssl miniupnpc
+brew install sdl2 sdl2_mixer freetype libpng jpeg-turbo glew fmt openssl miniupnpc catch2
 
 echo "CMAKE_PREFIX_PATH=$(brew --prefix)" >> $GITHUB_ENV
 
