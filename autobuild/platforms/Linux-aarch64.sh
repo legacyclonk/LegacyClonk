57a34c712b533ad10afbc39fb9f93783c6cbf478
Markus Mittendrein <github@maxmitti.tk>
2026-05-10 00:16:23 +0200

    Define GCC_ARCH in ci.toml

--- autobuild/platforms/Linux-aarch64.sh
+++ autobuild/platforms/Linux-aarch64.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-echo "GCC_PREFIX=aarch64-linux-gnu" >> $GITHUB_ENV
-echo "GCC_ARCH=aarch64" >> $GITHUB_ENV
+# nothing needed
--- autobuild/platforms/Linux-x64.sh
+++ autobuild/platforms/Linux-x64.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-echo "GCC_PREFIX=x86_64-linux-gnu" >> $GITHUB_ENV
-echo "GCC_ARCH=x86_64" >> $GITHUB_ENV
+# nothing needed
