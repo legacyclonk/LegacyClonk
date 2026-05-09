95d206bd428f597fbd9884d6b07fa42e63470a59
Markus Mittendrein <github@maxmitti.tk>
2026-05-09 14:26:22 +0200

    Fix Linux GCC path

--- autobuild/platforms/custom_gcc.cmake
+++ autobuild/platforms/custom_gcc.cmake
@@ -1,6 +1,6 @@
 cmake_minimum_required(VERSION 3.20)
-set(CMAKE_C_COMPILER "$ENV{GCC_PREFIX}-gcc-$ENV{GCC_VERSION}")
-set(CMAKE_CXX_COMPILER "$ENV{GCC_PREFIX}-g++-$ENV{GCC_VERSION}")
+set(CMAKE_C_COMPILER "$ENV{GCC_PREFIX}-gcc-$ENV{GCC_MAJOR}")
+set(CMAKE_CXX_COMPILER "$ENV{GCC_PREFIX}-g++-$ENV{GCC_MAJOR}")
 
 set(CMAKE_C_FLAGS_INIT "-static-libgcc")
 set(CMAKE_CXX_FLAGS_INIT "-static-libgcc -static-libstdc++")
--- autobuild/platforms/setup_linux.sh
+++ autobuild/platforms/setup_linux.sh
@@ -6,5 +6,7 @@ sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends zlib1g-dev libgl1-mesa-dev libjpeg-dev libpng-dev libssl-dev libglu1-mesa-dev libglew-dev libsdl2-dev libsdl2-mixer-dev libfreetype6-dev freeglut3-dev libxpm-dev libgtk-3-dev libnotify-dev cmake ninja-build git fuse -y
 
-curl -L "https://github.com/legacyclonk/gcc-build/releases/download/$GCC_VERSION/gcc-$GCC_VERSION-$GCC_ARCH.tar.zst" | sudo tar -x --zstd -C /
-echo "GCC_VERSION=$GCC_VERSION" >> $GITHUB_ENV
+curl -L "https://github.com/legacyclonk/gcc-build/releases/download/$GCC_VERSION/gcc-$GCC_VERSION-$GCC_ARCH.tar.zst" | sudo tar -x -v --zstd -C /
+
+GCC_MAJOR=$(echo "$GCC_VERSION" | cut -d . -f 1)
+echo "GCC_MAJOR=$GCC_MAJOR" >> $GITHUB_ENV
