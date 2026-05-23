f70a14e24eab7359c01a36246d47ba1f186d5e77
Markus Mittendrein <github@maxmitti.tk>
2026-05-23 22:26:40 +0200

    Mac x64: Target OSX 10.13

--- autobuild/platforms/Mac-x64.sh
+++ autobuild/platforms/Mac-x64.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "CMAKE_CONFIGURE_ARGS=$CMAKE_CONFIGURE_ARGS -DCMAKE_OSX_DEPLOYMENT_TARGET=13.3" >> $GITHUB_ENV
+echo "CMAKE_CONFIGURE_ARGS=$CMAKE_CONFIGURE_ARGS -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13" >> $GITHUB_ENV
 
-curl -L https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX10.9.sdk.tar.xz | tar xJ
-echo "OSX_SDK=$PWD/MacOSX10.9.sdk" >> $GITHUB_ENV
+curl -L https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX10.13.sdk.tar.xz | tar xJ
+echo "OSX_SDK=$PWD/MacOSX10.13.sdk" >> $GITHUB_ENV
