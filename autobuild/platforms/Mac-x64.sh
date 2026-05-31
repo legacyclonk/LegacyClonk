e27fa635b4e8c326f45416491663bdb7e7ab87e0
Markus Mittendrein <github@maxmitti.tk>
2026-05-31 19:08:09 +0200

    Mac: Target macOS 11.3 also for x64 and update to Clang 22

--- autobuild/platforms/Mac-x64.sh
+++ autobuild/platforms/Mac-x64.sh
@@ -1,23 +1,2 @@
 #!/bin/bash
-
-echo "CMAKE_CONFIGURE_ARGS=$CMAKE_CONFIGURE_ARGS -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13" >> $GITHUB_ENV
-
-curl -L "$BUILD_TOOL_URL/MacOSX10.13.sdk.tar.xz" | tar xJ
-
-echo "OSX_SDK=$PWD/MacOSX10.13.sdk" >> $GITHUB_ENV
-
-mkdir clang-21
-pushd clang-21
-
-curl -L "$BUILD_TOOL_URL/clang-21-21.1.8_1+analyzer.darwin_17.x86_64.tbz2" | tar --strip-components 3 -x -j
-
-LLVM_CXX_INCLUDE=$PWD/libexec/llvm-21/include/c++/v1
-LLVM_CXX_LIB=$PWD/libexec/llvm-21/lib/libc++
-
-echo "LLVM_CXX_INCLUDE=$LLVM_CXX_INCLUDE" >> $GITHUB_ENV
-echo "LLVM_CXX_LIB=$LLVM_CXX_LIB" >> $GITHUB_ENV
-
-LIBCLANG_DIR="$PWD/libexec/llvm-21/lib/clang/21/lib/darwin"
-echo "LIBCLANG_RT=$LIBCLANG_DIR/libclang_rt.profile_osx.a $LIBCLANG_DIR/libclang_rt.fuzzer_interceptors_osx.a $LIBCLANG_DIR/libclang_rt.fuzzer_osx.a $LIBCLANG_DIR/libclang_rt.osx.a $LIBCLANG_DIR/libclang_rt.cc_kext.a $LIBCLANG_DIR/libclang_rt.fuzzer_no_main_osx.a" >> $GITHUB_ENV
-
-popd
+# nothing needed
